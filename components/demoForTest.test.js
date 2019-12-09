a4dcc804ee5104f7696484cc265e4d78f8bf70e6
aarnadlr <aaronadler@gmail.com>
2019-12-09 10:26:43 -0500

    Move tests to /components

--- components/demoForTest.test.js
+++ components/demoForTest.test.js
@@ -2,7 +2,7 @@
 
 // import React from 'react';
 import { render } from '@testing-library/react';
-import DemoForTest from '../components/demoForTest';
+import DemoForTest from './demoForTest';
 
 
 describe('Demo With React Testing Library', () => {
--- next.config.js
+++ next.config.js
@@ -7,14 +7,5 @@ module.exports = withSass({
     // Reference a variable that was defined in the .env file
     // and make it available at Build Time
     TEST_VAR: process.env.TEST_VAR
-  },
-  webpack: (config, { dev }) => {
-    config.module.rules.push(
-      {
-        test: /\.test.js$/,
-        loader: 'ignore-loader'
-      }
-    );
-    return config;
   }
 });
